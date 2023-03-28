568bf9cec58fd470a187a6dbe856140c781ac18f
kilogrammie <117412844+kilogrammie@users.noreply.github.com>
2023-03-29 00:14:53 +0100

    Add files via upload

---
+++ src/App.svelte
@@ -0,0 +1,84 @@
+
+
+<br />
+<br />
+<br />
+
+<div class="px-5 col-12 col-lg-6 m-auto">
+  <center>
+    <img
+      class="mb-4"
+      src="https://media.istockphoto.com/id/1245268075/vector/sending-fast-mail-icon-newsletter-message-logo-vector.jpg?s=612x612&w=0&k=20&c=-OnmTfaCfo4uIVPO4n0C0jvY0t1xQik8zY7wD095ahM="
+      alt=""
+      width="72"
+      height="57"
+      style="display: flex; justify-content: center"
+    />
+  </center>
+  <h1 class="h3 mb-3 fw-bold">Mail Broadcast Board</h1>
+  <form
+    action="https://getform.io/f/bcaec947-6d26-4ae5-ab65-bc28e6613587"
+    method="POST"
+    enctype="multipart/form-data"
+  >
+    <div class="form-floating" style="margin-bottom: 10px">
+      <input
+        required
+        type="text"
+        class="form-control"
+        id="from"
+        placeholder="E.g Abcd selling senter"
+        name="Sender name"
+      />
+      <label for="floatingInput">Sender Name</label>
+    </div>
+    <div class="form-floating" style="margin-bottom: 10px">
+      <input
+        required
+        type="email"
+        class="form-control"
+        id="from"
+        placeholder="E.g Abcd selling senter"
+        name="Sender Email"
+      />
+      <label for="floatingInput">Sender Email</label>
+    </div>
+    <div class="form-floating" style="margin-bottom: 10px">
+      <input
+        required
+        type="text"
+        class="form-control"
+        placeholder="text"
+        style="min-height: 100px"
+        id="info"
+        name="Sender subject"
+      />
+      <label for="floatingPassword">Mail Subjections will goes here..</label>
+    </div>
+    <div class="my-2">NB:: fell free to malke your descriptions</div>
+    <div class="form-floating" style="margin-bottom: 10px">
+      <input
+        required
+        type="text"
+        class="form-control"
+        placeholder="text"
+        style="min-height: 200px"
+        id="phone"
+        name="Sender description"
+      />
+      <label for="floatingPassword">Mail Description will goes here.. </label>
+    </div>
+    <br />
+    <!-- add hidden Honeypot input to prevent spams -->
+    <input type="hidden" name="_gotcha" style="display:none !important" />
+
+    <div class="form-floating" style="margin-bottom: 10px">
+      <input type="file" name="file" class="form-control" />
+    </div>
+    <button class="w-100 btn btn-lg btn-primary" type="submit"> Send </button>
+  </form>
+
+  <p class="mt-5 mb-3 text-muted">
+    Nb: Make shure you fill all the neccesary input before submit
+  </p>
+</div>
